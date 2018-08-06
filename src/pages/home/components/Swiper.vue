e624516a9774d261504650f304301cc5cfdf6caf
gzb <1744998732@qq.com>
2018-08-07 03:43:40 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1807/22/382624cbe97b0502.jpg_750x200_36a074d6.jpg" />
+      </swiper-slide>
+       <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 31.25%
+  .swiper-img
+   width: 100%
+</style>
