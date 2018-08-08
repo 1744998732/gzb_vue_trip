bd4e4e8ab0e5d79965dce1b1ceab702f41daa4a9
gzb <1744998732@qq.com>
2018-08-08 22:20:29 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -7,6 +7,12 @@
        <swiper-slide>
         <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg" />
       </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1806/8f/d09fa241a01ac02.jpg_750x200_e0c86013.jpg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1807/40/1d7be74ed1584002.jpg_750x200_c41233c9.jpg" />
+      </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
 </div>
@@ -31,9 +37,8 @@ export default {
     background: #fff !important
 .wrapper
   overflow: hidden
-  width: 100%
-  height: 0
-  padding-bottom: 31.25%
+  width: 375xp
+  height: 100xp
   .swiper-img
    width: 100%
 </style>
